f08d38d99e3ed70f7e3a42bdc5623ddbbc9f91c0
niteshpollapally <niteshkumar1997@gmail.com>
2022-12-01 18:22:14 +0530

    updated

--- apple.sh
+++ apple.sh
@@ -1,2 +1,4 @@
- hello new update in the iphone is dynamic iland these is a cool feature that as apple as developed recently
+ hello new update in the iphone is dynamic inland these is a cool feature that as apple as developed recently
+
+ but the user facing issue to buy charges and airpods due to money issue
 
---
+++ nokia.sh
@@ -0,0 +1,3 @@
+nokia main moto was connecting people around the world 
+before internet exist in the world nokia phones were ruling overthe world
+but at present the nokia shutted down
