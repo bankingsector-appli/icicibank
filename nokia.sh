7eb5a84b6ac365fc1a0f22e618b13dc675f8f8f6
niteshpollapally <niteshkumar1997@gmail.com>
2022-12-01 18:25:04 +0530

    updating

--- nokia.sh
+++ nokia.sh
@@ -1,3 +1,4 @@
+ git cherry-pick demo
 nokia main moto was connecting people around the world 
 before internet exist in the world nokia phones were ruling overthe world
 but at present the nokia shutted down
