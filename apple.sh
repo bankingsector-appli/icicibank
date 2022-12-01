9d6db51490dc1e2eda438220036e461dec9dda75
niteshpollapally <niteshkumar1997@gmail.com>
2022-12-01 18:26:36 +0530

    done

--- apple.sh
+++ apple.sh
@@ -1,4 +1,4 @@
  hello new update in the iphone is dynamic inland these is a cool feature that as apple as developed recently
 
  but the user facing issue to buy charges and airpods due to money issue
-
+this is another line
