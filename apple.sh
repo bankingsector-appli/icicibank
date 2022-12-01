b35efa7136dd3336844dd741567457f1baaf0b4e
niteshpollapally <niteshkumar1997@gmail.com>
2022-12-01 15:47:50 +0530

    apple update

---
+++ apple.sh
@@ -0,0 +1,2 @@
+ hello new update in the iphone is dynamic iland these is a cool feature that as apple as developed recently
+
--- string.java
+++
@@ -1,8 +0,0 @@
-this file contains the string releated file
-
-hello good friends 
- 
-
-how are 
-i am updating this line in dev branch
-i am updating in master branch this code 
